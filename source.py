c978ad7c5645d018bd228d1141e3147d8371335d
Amir <125498036+syskkk@users.noreply.github.com>
2023-04-02 15:38:56 +0330

    Create source.py

---
+++ source.py
@@ -0,0 +1,77 @@
+# Get user input
+import math
+
+user_input = input('MiniCalculator V1.0.0'
+                   ' Say hello! ')
+
+# Check if the word "hello" is in the input
+if "hello" in user_input.lower():
+    print("Hello there!")
+else:
+    print("404")
+
+
+user_input = input('Enter a command:'
+                   '(subtraction,addition,multiplication,division,to the power of,radical...) ')
+
+if "subtraction" in user_input.lower():
+    subtraction1 = input('Give me number: ')
+    subtraction2 = input('Give me another number: ')
+    result = int(subtraction1) - int(subtraction2)
+    print(subtraction1 + ' minus ' + subtraction2 + ' is ')
+    print(result)
+
+if "addition" in user_input.lower():
+    subtraction1 = input('Give me number: ')
+    subtraction2 = input('Give me another number: ')
+    result = int(subtraction1) + int(subtraction2)
+    print(subtraction1 + ' plus ' + subtraction2 + ' is ')
+    print(result)
+
+if "multiplication" in user_input.lower():
+    subtraction1 = input('Give me number: ')
+    subtraction2 = input('Give me another number: ')
+    result = int(subtraction1) * int(subtraction2)
+    print(subtraction1 + ' times ' + subtraction2 + ' is ')
+    print(result)
+
+if "division" in user_input.lower():
+    subtraction1 = input('Give me number: ')
+    subtraction2 = input('Give me another number: ')
+    result = int(subtraction1) / int(subtraction2)
+    print(subtraction1 + ' divided by ' + subtraction2 + ' is ')
+    print(result)
+
+if "radical" in user_input.lower():
+    # Get user input
+    num = float(input("Give me number: "))
+
+    # Calculate the square root using the math.sqrt() function
+    sqrt = math.sqrt(num)
+
+    # Print the result
+    print("The square root of", num, "is", sqrt)
+
+
+if "to the power of" in user_input.lower():
+    subtraction1 = input('Give me number: ')
+    subtraction2 = input('Give me another number: ')
+    result = int(subtraction1) ** int(subtraction2)
+    print(subtraction1 + ' to the power of ' + subtraction2 + ' is ')
+    print(result)
+else:
+    input('You need to enter one of the commands mentioned above (subtraction,addition etc...)'
+          ' If you want to go back Go ahead and restart the app!If not,simply press enter to proceed.')
+
+input('Thanks for using MiniCalc by $YS! <3 Now lets play a little game.You will tell me when you were born(year),'
+      'and I will tell you how old you are! ')
+byear = input('What year were you born in? ')
+agg = 2023 - int(byear)
+print('You are ' + str(agg) + ' years old!')
+
+answer = input('Is that right? ')
+if 'yes' in answer.lower():
+    input('Very Well!You can close me by simply pressing Enter or clicking on the X on the top of the screen!')
+else:
+    input('Looks like an error may have occured with the calculation!or you just could not type yes properly.'
+          ' You can close the application by simply pressing ENTER on your keyboard. ')
